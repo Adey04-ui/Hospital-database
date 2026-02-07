f1db32e06f50dcee92ac3f6801e7c797ffc9fc45
Kenny Odukoya <kennyodukoya001@gmail.com>
2026-02-07 23:12:56 +0100

    added new features

--- customers-ui/src/components/MainEntrance.jsx
+++ customers-ui/src/components/MainEntrance.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function MainEntrance() {
   return (
     <div style={{display: 'flex', justifyContent: 'center', background: '#fff'}}>
-      <div style={{width: '65%', paddingBottom: '20px'}}>
+      <div style={{ paddingBottom: '20px'}} className='container-14'>
         <img src="/main-entrance-copy.jpg" alt="main_entrance" style={{width: '100%', opacity: '0.58'}} />
       </div>
     </div>
