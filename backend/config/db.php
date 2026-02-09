8186ac98632e83715848a2ebb259fe7c3634d74a
Kenny Odukoya <kennyodukoya001@gmail.com>
2026-02-09 21:55:48 +0100

    prepare php backend for hosting

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,6 +1,6 @@
 <?php
   require_once "../loadenv.php";
-  $conn = mysqli_connect(getenv("DB_HOST"),getenv("DB_USER"),getenv("DB_PASS"), getenv("DB_NAME"));
+  $conn = mysqli_connect(getenv("DB_HOST"),getenv("DB_USER"),getenv("DB_PASS"), getenv("DB_NAME"), getenv("DB_PORT" ?: 3306));
 
   if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
