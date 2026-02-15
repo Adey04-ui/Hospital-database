4cca6be82ac2d251b09255489941d7c9197572b4
Kenny Odukoya <kennyodukoya001@gmail.com>
2026-02-15 11:02:56 +0100

    added edit patients feature

--- backend/reviews/create.php
+++ backend/reviews/create.php
@@ -1,5 +1,28 @@
 <?php
-  require_once "../config/header.php";
+  $allowedOrigins = [
+      "http://localhost:5174",
+      ""
+  ];
+
+  if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+      header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+  }
+
+  header("Access-Control-Allow-Credentials: true");
+  header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+  header("Access-Control-Allow-Headers: Content-Type, x-requested-with");
+  header("Content-Type: application/json");
+
+  if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+      http_response_code(200);
+      exit;
+  }
+
+  session_set_cookie_params([
+    'samesite' => 'None',
+    'secure' => true,
+    'httponly' => true
+  ]);
   require_once "../config/db.php";
 
   $data = json_decode(file_get_contents("php://input"), true);
