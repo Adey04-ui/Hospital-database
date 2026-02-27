8ff3a3fd0ca863bc41e244ab996819e09f3b1e8b
Kenny Odukoya <kennyodukoya001@gmail.com>
2026-02-27 11:54:30 +0100

    frontend fix

--- customers-ui/src/App.jsx
+++ customers-ui/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <>
       <ScrollToTop />
-      <Topbar />
+      {/* <Topbar /> */}
       <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
