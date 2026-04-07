9f44531bbcd9dd48d9a7031eb0863c4a8da5a6ee
Kenny Odukoya <kennyodukoya001@gmail.com>
2026-04-08 00:13:09 +0100

    htaccess fix for render

--- backend/config/header.php
+++ backend/config/header.php
@@ -11,7 +11,7 @@ if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed
 
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, x-requested-with");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
