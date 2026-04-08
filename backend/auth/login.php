caad0bbe763ea143b44a416c06f71ea4ec509bbd
Kenny Odukoya <kennyodukoya001@gmail.com>
2026-04-08 02:38:18 +0100

    Fix Apache MPM conflict (final)

--- backend/auth/login.php
+++ backend/auth/login.php
@@ -1,15 +1,20 @@
-<?php 
-  require_once "../config/db.php";
-  require_once "../config/header.php";
-  require_once "../middleware/errorMiddleware.php";
-  session_start();
-  
-  $data = json_decode(file_get_contents("php://input"), true);
+<?php
+require_once "../config/header.php";
 
-  $email = mysqli_real_escape_string($conn, $data['email']) ?? "";
-  $password = mysqli_real_escape_string($conn, $data["password"]) ?? "";
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(200);
+  exit;
+}
+require_once "../config/db.php";
+require_once "../middleware/errorMiddleware.php";
+session_start();
 
-  $query = "
+$data = json_decode(file_get_contents("php://input"), true);
+
+$email = mysqli_real_escape_string($conn, $data['email']) ?? "";
+$password = mysqli_real_escape_string($conn, $data["password"]) ?? "";
+
+$query = "
   SELECT users.*, roles.name AS role
   FROM users
   JOIN roles ON users.role_id = roles.id
@@ -17,36 +22,36 @@
   LIMIT 1
   ";
 
-  $result = mysqli_query($conn, $query);
+$result = mysqli_query($conn, $query);
 
-  if($email == null) {
-    http_response_code(401);
-    echo json_encode(["message" => "no email"]);
-    exit;
-  }
+if ($email == null) {
+  http_response_code(401);
+  echo json_encode(["message" => "no email"]);
+  exit;
+}
 
-  if (mysqli_num_rows($result) === 0) {
-    http_response_code(401);
-    echo json_encode(["message" => "Invalid credentials, email doesnt exist '$email'"]);
-    exit;
-  }
+if (mysqli_num_rows($result) === 0) {
+  http_response_code(401);
+  echo json_encode(["message" => "Invalid credentials, email doesnt exist '$email'"]);
+  exit;
+}
 
-  $user = mysqli_fetch_assoc($result);
+$user = mysqli_fetch_assoc($result);
 
-  if (!password_verify($password, $user['password'])) {
-    http_response_code(401);
-    echo json_encode(["message" => "Invalid credentials, wrong password"]);
-    exit;
-  }
+if (!password_verify($password, $user['password'])) {
+  http_response_code(401);
+  echo json_encode(["message" => "Invalid credentials, wrong password"]);
+  exit;
+}
 
-  $_SESSION['user'] = [
-    "id" => $user['id'],
-    "role" => $user['role'],
-    "name" => $user['full_name']
-  ];
+$_SESSION['user'] = [
+  "id" => $user['id'],
+  "role" => $user['role'],
+  "name" => $user['full_name']
+];
 
-  echo json_encode([
-    "message" => "Login successful",
-    "user" => $_SESSION['user']
-  ]);
+echo json_encode([
+  "message" => "Login successful",
+  "user" => $_SESSION['user']
+]);
 ?>
