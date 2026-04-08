64b911fe48fa62e200e6d9c898dd36a7166a363f
Kenny Odukoya <kennyodukoya001@gmail.com>
2026-04-08 06:03:36 +0100

    Fix Apache MPM conflict (final)

--- backend/config/header.php
+++ backend/config/header.php
@@ -18,6 +18,13 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Content-Type: application/json");
 
+
+session_set_cookie_params([
+    'samesite' => 'None',
+    'secure' => true,
+    'httponly' => true
+]);
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200); // MUST be 200
     exit;
